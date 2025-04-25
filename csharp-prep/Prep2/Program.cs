c871f7260c0ab2baca1c1f310c2c490b818ecdb4
Isaac614 <moo24006@byui.edu>
2025-04-25 11:07:54 -0600

    completed prep1 and 2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string strGradePercent = Console.ReadLine();
+        int gradePercent = int.Parse(strGradePercent);
+
+        string letter;
+        if (gradePercent >= 90)
+        {
+            // Console.WriteLine("You have an A");
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            // Console.WriteLine("You have a B");
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            // Console.WriteLine("You have a C");
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            // Console.WriteLine("You have a D");
+            letter = "D";
+        }
+        else 
+        {
+            // Console.WriteLine("You have an F");
+            letter = "F";
+        }
+
+        Console.WriteLine(letter);
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congrats, you passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed the class");
+        }
     }
 }
