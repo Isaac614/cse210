7168403750001df7a796d53b6a7118cbb9cb26f3
Isaac614 <moo24006@byui.edu>
2025-04-26 13:31:11 -0600

    Completed Prep4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number?");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int userGuess = -1;
+
+        while (userGuess != magicNumber)
+        {
+            Console.WriteLine("\nWhat is your guess?");
+            userGuess = int.Parse(Console.ReadLine());
+            if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userInput = -1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine(numbers);
+        while (userInput != 0)
+        {
+            Console.WriteLine("Please enter a number");
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        int total = 0;
+        int largest = -1;
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+            total += num;
+        }
+        int average = total / numbers.Count();
+
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {largest}");
+
+
     }
 }
