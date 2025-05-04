d37a1a945511e686592ec9bf705eb84824fd2a24
Isaac614 <moo24006@byui.edu>
2025-05-03 20:06:19 -0600

    completed w02 prepare

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+public class Job
+    {
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void DisplayJobDetails()
+        {
+            Console.WriteLine(_jobTitle);
+            Console.WriteLine(_startYear);
+            Console.WriteLine(_endYear);
+        }
+
+        // public void person()
+        // {
+            
+        // }
+    }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2000;
+        job1._endYear = 2005;
+
+        Job job2 = new Job();
+        job2._jobTitle = "QA";
+        job2._startYear = 2000;
+        job2._endYear = 2010;
+
+        Resume resume = new Resume();
+        resume._name = "Isaac";
+        resume._jobList.Add(job1);
+        resume._jobList.Add(job2);
+
+        resume.Display();
+
+
     }
+
 }
